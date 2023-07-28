c4b6953e603a376688abc288734d2852b96dcf43
zjeffreys <zjeffreys@seattlu.edu>
2023-07-28 00:50:57 -0700

    login works!

--- backend/mysite/myapi/views.py
+++ backend/mysite/myapi/views.py
@@ -5,6 +5,8 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from .serializers import UserSerializer
+from rest_framework import status
+from django.contrib.auth import authenticate
 
 
 from .serializers import HeroSerializer
@@ -25,3 +27,15 @@ class UserLoginView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+    def list(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            serializer = self.get_serializer(user)
+            return Response(serializer.data)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
